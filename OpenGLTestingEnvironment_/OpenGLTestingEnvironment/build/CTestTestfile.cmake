b5ea1a1769b7b266a8c4a165dfbb6a803a5ee530
moltenpopsicle <mr.james.reggie@gmail.com>
2018-01-24 21:32:55 -0600

    removed useless build folder

--- OpenGLTestingEnvironment_/OpenGLTestingEnvironment/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/moltenpopsicle/media/projects/OpenDepictor/OpenGLTestingEnvironment_/OpenGLTestingEnvironment
-# Build directory: /home/moltenpopsicle/media/projects/OpenDepictor/OpenGLTestingEnvironment_/OpenGLTestingEnvironment/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
